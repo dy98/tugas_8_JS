38dd649ab139daee2d4886f4019c7f49296c407a
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 06:04:13 +0700

    Tugas 8 JS
    tugas Untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,15 @@
+// Buat Object
+let laptop = {
+	merk: 'Asus',
+	cpu: 'Intel',
+	gpu: 'Nvidia',
+	harga: 20000,
+	tahun: 2018
+}
+// tampilan awal
+console.log(laptop);
+console.log(laptop.harga);
+
+// tampilan perubahan
+laptop.harga = 6000000;
+console.log(laptop.harga);
